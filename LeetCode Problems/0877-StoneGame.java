cbeeb6ea0a60b832ef17b7cae77f4442700fea71
jiajunzh <jiajunzh@alumni.cmu.edu>
2022-07-11 17:09:04 -0400

    Add Problem 0486, 0877 and 1423

---
+++ LeetCode Problems/0877-StoneGame.java
@@ -0,0 +1,119 @@
+package problem;
+
+/**
+ * 1. Problem 
+ * Alice and Bob play a game with piles of stones. There are an even number of piles arranged in a row, and each pile 
+ * has a positive integer number of stones piles[i].
+ *
+ * The objective of the game is to end with the most stones. The total number of stones across all the piles is odd, 
+ * so there are no ties.
+ *
+ * Alice and Bob take turns, with Alice starting first. Each turn, a player takes the entire pile of stones either from
+ * the beginning or from the end of the row. This continues until there are no more piles left, at which point the 
+ * person with the most stones wins.
+ *
+ * Assuming Alice and Bob play optimally, return true if Alice wins the game, or false if Bob wins.
+ *
+ * 2. Examples 
+ * Example 1
+ * Input: piles = [5,3,4,5]
+ * Output: true
+ * Explanation: 
+ * Alice starts first, and can only take the first 5 or the last 5.
+ * Say she takes the first 5, so that the row becomes [3, 4, 5].
+ * If Bob takes 3, then the board is [4, 5], and Alice takes 5 to win with 10 points.
+ * If Bob takes the last 5, then the board is [3, 4], and Alice takes 4 to win with 9 points.
+ * This demonstrated that taking the first 5 was a winning move for Alice, so we return true.
+ * 
+ * Example 2
+ * Input: piles = [3,7,2,3]
+ * Output: true
+ *
+ * 3. Constraints
+ * 2 <= piles.length <= 500
+ * piles.length is even.
+ * 1 <= piles[i] <= 500
+ * sum(piles[i]) is odd.
+ */
+public class StoneGame {
+  
+  private int[][] memo;
+
+  /**
+   * 1. Approach 
+   * Memoization + MinMax.
+   * 
+   * Define a relative score which will increase by piles[i] if it is Alice turn otherwise decrease by piles[i]. 
+   * Then score(i, j) = max(score(i + 1, j) + piles[i], score(i, j - 1) + piles[j])
+   * 
+   * 2. Complexity
+   * - Time O(N^2)
+   * - Space O(N^2)
+   * 
+   * @param piles
+   * @return
+   */
+  public boolean stoneGame1(int[] piles) {
+    memo = new int[piles.length][piles.length];
+    return score(piles, 0, piles.length - 1) > 0;
+  }
+
+  private int score(int[] piles, int start, int end) {
+    if (start > end) return 0;
+    if (start == end) return piles[start];
+    if (memo[start][end] != 0) return memo[start][end];
+    memo[start][end] = Math.max(piles[start] + score(piles, start + 1, end), piles[end] + score(piles, start, end - 1));
+    return memo[start][end];
+  }
+
+  /**
+   * 1. Approach 
+   * 2D Dynamic Programming + MinMax
+   * 
+   * 2. Complexity 
+   * - Time O(N^2)
+   * - Space O(N^2)
+   * 
+   * @param piles
+   * @return
+   */
+  public boolean stoneGame2(int[] piles) {
+    final int n = piles.length;
+    final int[][] dp = new int[n][n];
+    for (int i = 0; i < n; i++) {
+      dp[i][i] = piles[i];
+    }
+    for (int size = 2; size <= n; size++) {
+      for (int i = 0; i + size <= n; i++) {
+        int j = i + size - 1;
+        dp[i][j] = Math.max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
+      }
+    }
+    return dp[0][n - 1] > 0;
+  }
+
+  /**
+   * 1. Approach 
+   * 1D Dynamic Programming + MinMax
+   *
+   * 2. Complexity 
+   * - Time O(N^2)
+   * - Space O(N)
+   *
+   * @param piles
+   * @return
+   */
+  public boolean stoneGame3(int[] piles) {
+    final int n = piles.length;
+    final int[] dp = new int[n];
+    for (int i = 0; i < n; i++) {
+      dp[i] = piles[i];
+    }
+    for (int size = 2; size <= n; size++) {
+      for (int i = 0; i + size <= n; i++) {
+        dp[i] = Math.max(piles[i] - dp[i + 1], piles[i + size - 1] - dp[i]);
+      }
+    }
+    return dp[0] > 0;
+  }
+}
---
+++ LeetCode Problems/1423-MaximumPointsYouCanObtainFromCards.java
@@ -0,0 +1,69 @@
+package problem;
+
+/**
+ * 1. Problem 
+ * There are several cards arranged in a row, and each card has an associated number of points. The points are given 
+ * in the integer array cardPoints.
+ *
+ * In one step, you can take one card from the beginning or from the end of the row. You have to take exactly k cards.
+ *
+ * Your score is the sum of the points of the cards you have taken.
+ *
+ * Given the integer array cardPoints and the integer k, return the maximum score you can obtain.
+ *
+ * 2. Examples 
+ * Example 1
+ * Input: cardPoints = [1,2,3,4,5,6,1], k = 3
+ * Output: 12
+ * Explanation: After the first step, your score will always be 1. However, choosing the rightmost card first will
+ * maximize your total score. The optimal strategy is to take the three cards on the right, giving a final score 
+ * of 1 + 6 + 5 = 12.
+ *
+ * Example 2
+ * Input: cardPoints = [2,2,2], k = 2
+ * Output: 4
+ * Explanation: Regardless of which two cards you take, your score will always be 4.
+ * 
+ * Example 3
+ * Input: cardPoints = [9,7,7,9,7,7,9], k = 7
+ * Output: 55
+ * Explanation: You have to take all the cards. Your score is the sum of points of all cards.
+ *
+ * 3. Constraints
+ * 1 <= cardPoints.length <= 105
+ * 1 <= cardPoints[i] <= 104
+ * 1 <= k <= cardPoints.length
+ */
+public class MaximumPointsYouCanObtainFromCards {
+
+  /**
+   * 1. Approach 
+   * Dynamic Programming 
+   * 
+   * 2. Complexity 
+   * - Time O(K)
+   * - Space O(1)
+   * 
+   * 3. Alternative 
+   * - Another way to look at this is Max(Score) == Min(Sum[i:j]) where array[i:j] contains n - k elements. Sliding 
+   *   window could be used here.
+   * 
+   * @param cardPoints
+   * @param k
+   * @return
+   */
+  public int maxScore(int[] cardPoints, int k) {
+    final int n = cardPoints.length;
+    int currScore = 0;
+    for (int i = 0; i < k; i++) {
+      currScore += cardPoints[i];
+    }
+    int maxScore = currScore;
+    for (int i = k - 1; i >= 0; i--) {
+      currScore -= cardPoints[i];
+      currScore += cardPoints[n - k + i];
+      maxScore = Math.max(currScore, maxScore);
+    }
+    return maxScore;
+  }
+}

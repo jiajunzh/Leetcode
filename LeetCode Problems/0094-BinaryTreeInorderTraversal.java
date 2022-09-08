001284eac4f113e0564ef6ca2d55ddd57ec5fdbf
jiajunzh <jiajunzh@alumni.cmu.edu>
2022-09-07 23:40:19 -0400

    Add Problem 0094

--- LeetCode Problems/0094-BinaryTreeInorderTraversal.java
+++ LeetCode Problems/0094-BinaryTreeInorderTraversal.java
@@ -4,104 +4,122 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
+/**
+ * 1. Problem 
+ * Given the root of a binary tree, return the inorder traversal of its nodes' values.
+ *
+ * 2. Examples
+ * Example 1
+ * Input: root = [1,null,2,3]
+ * Output: [1,3,2]
+ * 
+ * Example 2
+ * Input: root = []
+ * Output: []
+ * 
+ * Example 3
+ * Input: root = [1]
+ * Output: [1]
+ *
+ * 3. Constraints
+ * The number of nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ *
+ * 4. Follow up
+ * Recursive solution is trivial, could you do it iteratively?
+ */
 public class BinaryTreeInorderTraversal {
-  
-  public static List<Integer> inorderTraversalDfs(TreeNode root) {
+
+  /**
+   * 1. Approach 
+   * Recursion 
+   * 
+   * 2. Complexity 
+   * - Time O(N)
+   * - Space O(N)
+   * 
+   * @param root
+   * @return
+   */
+  public List<Integer> inorderTraversal(TreeNode root) {
     final List<Integer> result = new ArrayList<>();
-
-    /**
-     * PreOrder Traversal: root left right 
-     * InOrder Traversal: left root right 
-     * PostOrder Traversal: left right root
-     */
-    inorderTraversalHelper(root, result);
-
+    inorderTraversal(root, result);
     return result;
   }
 
-  private static void inorderTraversalHelper(final TreeNode root, final List<Integer> result) {
-    if (root == null) {
-      return;
-    }
-
-    inorderTraversalHelper(root.left, result);
-    visit(root, result);
-    inorderTraversalHelper(root.right, result);
-  }
-
-  public static List<Integer> inorderTraversalIterative(TreeNode root) {
-    final List<Integer> result = new ArrayList<>();
-
-    if (root == null) {
-      return result;
-    }
-
-    final Stack<TreeNode> stack = new Stack<>();
-
-    stack.push(root);
-    TreeNode currentNode = root.left;
-
-    while (true) {
-      if (currentNode != null) {
-        stack.push(currentNode);
-        currentNode = currentNode.left;
-      } else {
-        if (stack.isEmpty()) {
-          break;
-        }
-        final TreeNode currentRoot = stack.pop();
-        visit(currentRoot, result);
-        currentNode = currentRoot.right;
-      }
-    }
-
-    return result;
-  }
-
-  private static void visit(final TreeNode root, final List<Integer> result) {
+  public void inorderTraversal(TreeNode root, List<Integer> result) {
+    if (root == null) return;
+    inorderTraversal(root.left, result);
     result.add(root.val);
-  }
-  
-  public static void main(String[] args) {
-    final TreeNode root = constructTree();
-    
-    System.out.println("In-Order Traversal using DFS.");
-    System.out.println(inorderTraversalDfs(root));
-    System.out.print("\n");
-
-    System.out.println("In-Order Traversal using iterative approach.");
-    System.out.println(inorderTraversalIterative(root));
-    System.out.print("\n");
+    inorderTraversal(root.right, result);
   }
 
   /**
-   * Construct a tree as below.
-   *
-   *
-   *          10  
-   *     15       30
-   *   3    6        2
-   * 5             9   8
-   *
+   * 1. Approach 
+   * Iteration with Stack
+   * 
+   * 2. Complexity 
+   * - Time O(N)
+   * - Space O(N)
+   * 
+   * @param root
    * @return
    */
-  private static TreeNode constructTree() {
-    return new TreeNode(10,
-      new TreeNode(15,
-        new TreeNode(3, new TreeNode(5), null),
-        new TreeNode(6, null, null)
-      ),
-      new TreeNode(30,
-        null,
-        new TreeNode(2,
-          new TreeNode(9),
-          new TreeNode(8)
-        )
-      )
-    );
+  public List<Integer> inorderTraversal2(TreeNode root) {
+    final List<Integer> result = new ArrayList<>();
+    final Stack<TreeNode> stack = new Stack<>();
+    TreeNode curr = root;
+    while (curr != null || !stack.isEmpty()) {
+      while (curr != null) {
+        stack.push(curr);
+        curr = curr.left;
+      }
+      curr = stack.pop();
+      result.add(curr.val);
+      curr = curr.right;
+    }
+    return result;
   }
 
-  public static class TreeNode {
+  /**
+   * 1. Approach 
+   * Morris Traversal. The reason of having stack in regular traversal is to keep track of the previous nodes so that
+   * we could traverse back later on. Morris traversal finds a way of traversing back by pointing rightmos node (predecessor) 
+   * back to the current visited node, which eliminates the needs of stack.
+   * 
+   * 2. Complexity 
+   * - Time O(N)
+   * - Space O(1)
+   * 
+   * @param root
+   * @return
+   */
+  public List<Integer> inorderTraversal3(TreeNode root) {
+    final List<Integer> result = new ArrayList<>();
+    TreeNode curr = root;
+    while (curr != null) {
+      if (curr.left == null) {
+        result.add(curr.val);
+        curr = curr.right;
+      } else {
+        TreeNode predecessor = curr.left;
+        while (predecessor.right != null && predecessor.right != curr) {
+          predecessor = predecessor.right;
+        }
+        if (predecessor.right == null) {
+          predecessor.right = curr;
+          curr = curr.left;
+        } else {
+          predecessor.right = null;
+          result.add(curr.val);
+          curr = curr.right;
+        }
+      }
+    }
+    return result;
+  }
+
+  private static class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;

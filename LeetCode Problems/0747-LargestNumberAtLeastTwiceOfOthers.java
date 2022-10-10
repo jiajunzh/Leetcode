4ee159fd43b3c54bb60416b7cae1124bfc0a5364
jiajunzh <jiajunzh@alumni.cmu.edu>
2022-10-10 13:54:48 -0400

    Add Problem 0066, 0724 and 0747

---
+++ LeetCode Problems/0747-LargestNumberAtLeastTwiceOfOthers.java
@@ -0,0 +1,54 @@
+package problem;
+
+/**
+ * 1. Problem
+ * You are given an integer array nums where the largest integer is unique.
+ *
+ * Determine whether the largest element in the array is at least twice as much as every other number in the array. 
+ * If it is, return the index of the largest element, or return -1 otherwise.
+ *
+ * 2. Examples
+ * Example 1
+ * Input: nums = [3,6,1,0]
+ * Output: 1
+ * Explanation: 6 is the largest integer.
+ * For every other number in the array x, 6 is at least twice as big as x.
+ * The index of value 6 is 1, so we return 1.
+ * 
+ * Example 2
+ * Input: nums = [1,2,3,4]
+ * Output: -1
+ * Explanation: 4 is less than twice the value of 3, so we return -1.
+ *
+ * 3. Constraints
+ * 2 <= nums.length <= 50
+ * 0 <= nums[i] <= 100
+ * The largest element in nums is unique.
+ */
+public class LargestNumberAtLeastTwiceOfOthers {
+
+  /**
+   * 1. Approach
+   * Array One Pass 
+   * 
+   * 2. Complexity
+   * - Time O(N)
+   * - Space O(1)
+   * 
+   * @param nums
+   * @return
+   */
+  public int dominantIndex(int[] nums) {
+    int prevMax = -1, max = -1, maxIndex = -1;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] > max) {
+        prevMax = max;
+        max = nums[i];
+        maxIndex = i;
+      } else {
+        prevMax = Math.max(prevMax, nums[i]);
+      }
+    }
+    return nums[maxIndex] >= 2 * prevMax ? maxIndex : -1;
+  }
+}

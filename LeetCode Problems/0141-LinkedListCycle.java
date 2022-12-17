3a2dc31ade13b2fddf56d866098aac284bb0a69b
jiajunzh <jiajunzh@alumni.cmu.edu>
2022-12-16 22:11:24 -0500

    Add Problem 0141

---
+++ LeetCode Problems/0141-LinkedListCycle.java
@@ -0,0 +1,69 @@
+package problem;
+
+/**
+ * 1. Problem
+ * Given head, the head of a linked list, determine if the linked list has a cycle in it.
+ *
+ * There is a cycle in a linked list if there is some node in the list that can be reached again by continuously
+ * following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is
+ * connected to. Note that pos is not passed as a parameter.
+ *
+ * Return true if there is a cycle in the linked list. Otherwise, return false.
+ *
+ * 2. Examples
+ * Example 1
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+ *
+ * Example 2
+ * Input: head = [1,2], pos = 0
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+ *
+ * Example 3
+ * Input: head = [1], pos = -1
+ * Output: false
+ * Explanation: There is no cycle in the linked list.
+ *
+ * 3. Constraints
+ * The number of the nodes in the list is in the range [0, 104].
+ * -105 <= Node.val <= 105
+ * pos is -1 or a valid index in the linked-list.
+ *
+ * 4. Follow up
+ * Can you solve it using O(1) (i.e. constant) memory?
+ */
+public class LinkedListCycle {
+
+    /**
+     * 1. Approach
+     * Floyd's Cycle (Slow/Fast Runner)
+     *
+     * 2. Complexity
+     * - Time O(N)
+     * - Space O(1)
+     *
+     * @param head
+     * @return
+     */
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        ListNode slowRunner = head, fastRunner = head.next;
+        while (slowRunner != fastRunner) {
+            if (fastRunner == null || fastRunner.next == null) return false;
+            slowRunner = slowRunner.next;
+            fastRunner = fastRunner.next.next;
+        }
+        return true;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
